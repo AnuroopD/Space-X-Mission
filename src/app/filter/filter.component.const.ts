cc8c9e3dbac45a7da2cb52630a9bb2def548f2f0
AnuroopD <anuroopdeshpande.work@gmail.com>
2021-01-24 20:12:11 +0530

    Added Toggle features and also few bug fixes

--- src/app/filter/filter.component.const.ts
+++ src/app/filter/filter.component.const.ts
@@ -1,21 +1,31 @@
 export const EVEN_YEARS = [
-    "2006",
-    "2008",
-    "2010",
-    "2012",
-    "2014",
-    "2016",
-    "2018",
-    "2020"
+    { value: "2006", isActive: false },
+    { value: "2008", isActive: false },
+    { value: "2010", isActive: false },
+    { value: "2012", isActive: false },
+    { value: "2014", isActive: false },
+    { value: "2016", isActive: false },
+    { value: "2018", isActive: false },
+    { value: "2020", isActive: false }
 
-]
+];
 
 export const ODD_YEARS = [
-    "2007",
-    "2009",
-    "2011",
-    "2013",
-    "2015",
-    "2017",
-    "2019"
-]
+    { value: "2007", isActive: false },
+    { value: "2009", isActive: false },
+    { value: "2011", isActive: false },
+    { value: "2013", isActive: false },
+    { value: "2015", isActive: false },
+    { value: "2017", isActive: false },
+    { value: "2019", isActive: false }
+];
+
+export const LAUNCH = [
+    { value: "true", isActive: false },
+    { value: "false", isActive: false },
+];
+
+export const LAND = [
+    { value: "true", isActive: false },
+    { value: "false", isActive: false },
+];
--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Output } from '@angular/core';
-import { EVEN_YEARS, ODD_YEARS } from './filter.component.const';
+import { AfterViewInit, Component, OnInit, Output } from '@angular/core';
+import { EVEN_YEARS, LAND, LAUNCH, ODD_YEARS } from './filter.component.const';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -7,10 +7,16 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent implements OnInit, AfterViewInit {
   messageToParent: any = "Hello Parent!!";
   missionEvenYears = EVEN_YEARS;
   missionOddYears = ODD_YEARS;
+  missionLaunch = LAUNCH;
+  missionLand = LAND;
+  status: boolean = false;
+  buttonClicked: boolean;
+  queryParamArray = [];
+  buttonClass: any;
   constructor(
     private router: Router,
     private route: ActivatedRoute
@@ -18,10 +24,62 @@ export class FilterComponent implements OnInit {
 
 
   ngOnInit(): void {
+    this.updateIsActiveOnInit();
   }
 
-  filterByParams(queryParams: { [key: string]: any }) {
+  ngAfterViewInit(): void {
+
+  }
+
+  updateIsActiveOnInit() {
+    this.route.queryParams.subscribe(queryParam => {
+      const launchYear = queryParam.launch_year;
+      const launch = queryParam.launch_success;
+      const land = queryParam.land_success;
+      const yearArray = this.missionEvenYears.concat(this.missionOddYears);
+      const launchYearObj = yearArray.find(val => val.value === launchYear);
+      launchYearObj ? (launchYearObj.isActive = true) : '';
+      const launchObj = this.missionLaunch.find(val => val.value === launch);
+      launchObj ? (launchObj.isActive = true) : '';
+      const landObj = this.missionLand.find(val => val.value === land);
+      landObj ? (landObj.isActive = true) : '';
+    });
+
+  }
+  filterByParams(queryParamName: string, queryObj?) {
+
+    const isActive = queryObj.isActive;
+    queryObj.isActive = !isActive;
+    const queryParams = {};
+    queryParams[queryParamName] = !isActive ? queryObj.value : null;
     this.router.navigate(['/'], { relativeTo: this.route, queryParams: queryParams, queryParamsHandling: 'merge' });
   }
 
+  updateIsActiveClass(year) {
+    this.missionEvenYears.forEach((val) => {
+      if (val.value !== year) {
+        val.isActive = false;
+      }
+    });
+    this.missionOddYears.forEach((val) => {
+      if (val.value !== year) {
+        val.isActive = false;
+      }
+    })
+  }
+  updateIsActive(methodName, index) {
+    this[methodName].forEach((val, i) => {
+      if (i !== index) {
+        val.isActive = false;
+      }
+    })
+  }
 }
+
+
+
+// this[methodName].forEach((val, i) => {
+//   if (i !== index) {
+//     val.isActive = false;
+//   }
+// })
